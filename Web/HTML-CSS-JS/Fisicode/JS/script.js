7ce7a28f028c25a0cb0f14de1b45885e183d5987
Lucas <lucassantana.github@gmail.com>
2026-04-27 12:27:50 +0000

    atualizando botoes fisicode

--- Web/HTML-CSS-JS/Fisicode/JS/script.js
+++ Web/HTML-CSS-JS/Fisicode/JS/script.js
@@ -1,49 +1,43 @@
-render()
+render();
 
 function render(){
     const espacoCard=document.getElementById("espacoCard")
 
-    espacoCard.innerHTML=`<div class="card">
-        <div class="espacoInput">
-
-             <input id="v0" type="number" placeholder="Insira a Velocidade Inicial">
-             <input id="a" type="number" placeholder="Insira a Aceleração">
-             <input id="d" type="number" placeholder="Insira a Distância">
-            
-        </div>
-
-       <div class="espacoResposta">
-    <textarea id="campoResposta" readonly>
-      Velocidade Final:
-
-    </textarea>
+    espacoCard.innerHTML=`
+    <div class="card">
     
-</div>
+    <div class="espacoInput">
+        <input id="v0" type="number" placeholder="Insira a Velocidade Inicial">
+        <input id="a" type="number" placeholder="Insira a Aceleração">
+        <input id="d" type="number" placeholder="Insira a Distância">
+    </div>
 
-        <div class="espacoBotao">
-
-            <button class="btn-operacao"  onclick="calcular()">Calcular</button>
-            
+    <div class="espacoResposta">
+        <div id="campoResposta">
+            Velocidade Final:
         </div>
+    </div>
 
-        
-    </div>` 
-}
+    <!-- BOTÃO TEM QUE FICAR AQUI DENTRO -->
+    <div class="espacoBotao">
+        <button class="btn-operacao" onclick="calcular()">Calcular</button>
+    </div>
 
-
+</div>`
+};
 
 function calcular(){
-    let campoResposta=document.getElementById("campoResposta")
-    let v0= document.getElementById("v0").value 
-     let a= document.getElementById("a").value 
-     let d= document.getElementById("d").value 
+    let campoResposta=document.getElementById("campoResposta");
+    let v0= document.getElementById("v0").value;
+     let a= document.getElementById("a").value;
+     let d= document.getElementById("d").value;
 
 
-     var Vquadrado = Math.sqrt( (v0 **2) + 2 * a *d )
+     var Vquadrado = Math.sqrt( (v0 **2) + 2 * a *d );
 
      
 
-     campoResposta.innerHTML=(Vquadrado+"M/s")
+     campoResposta.innerHTML=(Vquadrado+"M/s");
 
 
     
